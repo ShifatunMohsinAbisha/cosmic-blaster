fa47851f43727207ee65002ad766f0eda2da580d
ShifatunMohsinAbisha <abishashifatun@gmail.com>
2026-05-06 21:31:51 +0600

    Added player spaceship

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ screen.title("Cosmic Blaster")
 screen.bgcolor("black")
 screen.setup(width=700, height=600)
 
+player = turtle.Turtle()
+player.shape("triangle")
+player.color("cyan")
+player.penup()
+player.setheading(90)
+player.goto(0, -250)
+
 turtle.done()
